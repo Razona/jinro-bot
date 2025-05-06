edaf59de65f8a782b2c014b5e07f2918e97be7bc
Razona <razona0718@gmail.com>
2025-05-06 20:32:18 +0900

    user-listのモックを作成

--- hobu_bot.js
+++ hobu_bot.js
@@ -19,3 +19,25 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html')); // index.htmlを返す
 }
 );
+
+let userList = [
+  { "discordId": "123456789012345678", "screenName": "ユーザーA", "playerNumber": 1 },
+  { "discordId": "987654321098765432", "screenName": "ユーザーB", "playerNumber": 2 },
+  { "discordId": "112233445566778899", "screenName": "ユーザーC", "playerNumber": 3 },
+  { "discordId": "998877665544332211", "screenName": "ユーザーD", "playerNumber": 4 },
+  { "discordId": "223344556677889900", "screenName": "ユーザーE", "playerNumber": 5 },
+  { "discordId": "334455667788990011", "screenName": "ユーザーF", "playerNumber": 6 },
+  { "discordId": "445566778899001122", "screenName": "ユーザーG", "playerNumber": 7 },
+  { "discordId": "556677889900112233", "screenName": "ユーザーH", "playerNumber": 8 },
+  { "discordId": "667788990011223344", "screenName": "ユーザーI", "playerNumber": 9 },
+  { "discordId": "778899001122334455", "screenName": "ユーザーJ", "playerNumber": 10 },
+  { "discordId": "889900112233445566", "screenName": "ユーザーK", "playerNumber": 11 },
+  { "discordId": "990011223344556677", "screenName": "ユーザーL", "playerNumber": 12 },
+  { "discordId": "101112131415161718", "screenName": "ユーザーM", "playerNumber": 13 }
+];
+
+app.get('user-list/pull ', (req, res) => {
+  //json形式で返す
+  res.json(userList);
+}
+);
